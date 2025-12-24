e37306dbe8587dbf40494ff503459ea82b7ab4b7
Rakesh Gupta <rakeshgupta1361411@gmail.com>
2025-12-25 01:10:13 +0530

    password

--- src/main/java/com/ecommerce/project/util/PasswordValidator.java
+++ src/main/java/com/ecommerce/project/util/PasswordValidator.java
@@ -2,47 +2,17 @@ package com.ecommerce.project.util;
 
 import com.ecommerce.project.exception.BadRequestException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PasswordValidator {
 
-    private static final int MIN_LENGTH = 8;
-    private static final int MAX_LENGTH = 128;
+    private static final int MIN_LENGTH = 6;
 
     public static void validate(String password) {
         if (password == null || password.isEmpty()) {
             throw new BadRequestException("Password cannot be empty");
         }
 
-        List<String> errors = new ArrayList<>();
-
         if (password.length() < MIN_LENGTH) {
-            errors.add("Password must be at least " + MIN_LENGTH + " characters long");
-        }
-
-        if (password.length() > MAX_LENGTH) {
-            errors.add("Password must not exceed " + MAX_LENGTH + " characters");
-        }
-
-        if (!password.matches(".*[A-Z].*")) {
-            errors.add("Password must contain at least one uppercase letter");
-        }
-
-        if (!password.matches(".*[a-z].*")) {
-            errors.add("Password must contain at least one lowercase letter");
-        }
-
-        if (!password.matches(".*\\d.*")) {
-            errors.add("Password must contain at least one digit");
-        }
-
-        if (!password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*")) {
-            errors.add("Password must contain at least one special character");
-        }
-
-        if (!errors.isEmpty()) {
-            throw new BadRequestException("Password validation failed: " + String.join(", ", errors));
+            throw new BadRequestException("Password must be at least " + MIN_LENGTH + " characters long");
         }
     }
 
